d5bd8676543d7766a1d8c6463cad672bc6a90b1c
Thiago Mônaco <thiagomonaco98@gmail.com>
2024-07-31 16:48:57 -0300

    add already have an account button

--- lib/chat_web/live/create_account/create_account_live.ex
+++ lib/chat_web/live/create_account/create_account_live.ex
@@ -29,24 +29,28 @@ defmodule ChatWeb.CreateAccount.CreateAccountLive do
 
   def render(assigns) do
     ~H"""
-      <div class="container">
+      <div class="container flex flex-col items-center gap-6">
         <h1 class="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
           Create an account 
         </h1>
         <.form 
           phx-submit="save" 
           phx-change="validate"
-          class="flex w-full items-center flex-col mt-10 text-xl gap-6"
+          class="flex w-full items-center flex-col text-xl gap-6"
           for={@form}>
             <div class="flex sm:w-1/2 w-full justify-center flex-col gap-6">
               <.input phx-debounce="blur" label="Name" field={@form[:name]} placeholder="Name" />
               <.input phx-debounce="blur" label="Email" field={@form[:email]} placeholder="Email" />
-              <.input phx-debounce="blur" label="Password" field={@form[:password]} placeholder="Password" />
+              <.input type="password" phx-debounce="blur" label="Password" field={@form[:password]} placeholder="Password" />
             </div>
             <.button> 
               Create account
             </.button>
         </.form>
+        <.link class="text-lg flex flex-col items-center group">
+          Already have an account  
+          <div class="w-1/2 bg-gray-900 h-1.5 rounded transition-all duration-700 group-hover:w-full group-hover:bg-yellow-400"/>
+        </.link>
       </div>
     """
   end
